04ebc375bb97221b3c40ab3a6ecd7927be60af4c
Larry Miles <lmiles@lundinoir.com>
2018-03-20 17:15:34 +0000

    remove documents update

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -99,7 +99,21 @@ function on2() {
     document.getElementById("overlay2").style.display = "block";
 }
 
+function on3() {
+    document.getElementById("overlay3").style.display = "block";
+}
+
+function on4() {
+    document.getElementById("overlay4").style.display = "block";
+}
+
 function off() {
     document.getElementById("overlay").style.display = "none";
     document.getElementById("overlay2").style.display = "none";
+}
+function off3() {
+    document.getElementById("overlay3").style.display = "none";
+}
+function off4() {
+    document.getElementById("overlay4").style.display = "none";
 }
