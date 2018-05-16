043e348553f4da52f9170541730ec78fd3e94184
Larry Miles <lmiles@lundinoir.com>
2018-05-16 19:02:10 +0100

    defendant details changed

--- app/routes.js
+++ app/routes.js
@@ -38,7 +38,7 @@ router.post('/', function (req, res) {
     req.session.data['defendant-telephone'] = ""  
     req.session.data['defendant-mobile'] = "07975337198"
     
-    req.session.data['defendant-nin'] = "VO 12 3445 56 D"
+    req.session.data['defendant-nin'] = "VO 12 34 56 D"
     req.session.data['defendant-net-income'] = "1000"
     req.session.data['defendant-income-frequency-group'] = "Monthly"
     req.session.data['defendant-employment-status-group'] = "Employed (full or part-time)"
@@ -53,6 +53,46 @@ router.post('/', function (req, res) {
     req.session.data['statement-of-facts'] = "I, Inspector Morse, was suspicious as the defendant kept moving seats away from me. When asked for a ticket he replied “I don’t have one as I am not really on this train, you’re seeing an illusion”. I had no choice but to issue him with a ticket."
     
     req.session.data['case-notes'] = "No case notes have been added."
+
+    // ************************************
+    //CASE DETAILS 2 - TRANSPORT FOR LONDON
+    req.session.data['case-reference-number-2'] = "9016435"
+    req.session.data['case-status-2'] = "Completed"
+    req.session.data['prosecutor'] = "Transport for London"
+    req.session.data['notice-served-2'] = "12 February 2018"
+    req.session.data['notice-served-short-2'] = "12 Feb 2018"
+
+    req.session.data['defendant-title-2'] = ""
+    req.session.data['defendant-first-name-2'] = "Jerry"
+    req.session.data['defendant-last-name-2'] = "Smith"
+    req.session.data['defendant-dob-day-2'] = "17"
+    req.session.data['defendant-dob-month-2'] = "6"
+    req.session.data['defendant-dob-year-2'] = "1983"    
+    req.session.data['defendant-age-2'] = 2018-req.session.data['defendant-dob-year-2']   
+    req.session.data['defendant-address-line-1-2'] = "38 Brookside Close"    
+    req.session.data['defendant-address-line-2-2'] = ""    
+    req.session.data['defendant-address-line-3-2'] = ""    
+    req.session.data['defendant-address-city-2'] = "London"    
+    req.session.data['defendant-address-postcode-2'] = "NW19 7SA"  
+    req.session.data['defendant-email-2'] = "jennyanydots@hotmail.com"  
+    req.session.data['defendant-telephone-2'] = "0203 543 8928"  
+    req.session.data['defendant-mobile-2'] = ""
+    
+    req.session.data['defendant-nin-2'] = "BA 76 54 32 C"
+    req.session.data['defendant-net-income-2'] = "375"
+    req.session.data['defendant-income-frequency-group-2'] = "Weekly"
+    req.session.data['defendant-employment-status-group-2'] = "Employed (full or part-time)"
+    req.session.data['defendant-claiming-benefits-group-2'] = "No"
+    
+    req.session.data['defendant-plea-2'] = "Not guilty"
+    req.session.data['defendant-mitigation-2'] = "Lorem ipsum dolor sit amet"
+    
+    req.session.data['offence-title-2'] = "Passenger used ticket issued for another person"
+    req.session.data['offence-description-2'] = "On 10/06/2014 At grosvenor place SW1 Being a passenger on a Public Service Vehicle operated on behalf of London Bus Services Limited being used for the carriage of passengers at separate fares where the vehicle was being operated by a Driver without a Conductor did not immediately on boarding pay the fare to the Driver for the journey you intended to take."
+    
+    req.session.data['statement-of-facts-2'] = "I, Inspector Morse, was suspicious as the defendant kept moving seats away from me. When asked for a ticket he replied “I don’t have one as I am not really on this train, you’re seeing an illusion”. I had no choice but to issue him with a ticket."
+    
+    req.session.data['case-notes-2'] = "No case notes have been added."
 
     res.redirect('/courtadmin/sign-in')
     
